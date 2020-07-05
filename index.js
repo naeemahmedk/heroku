4f0ca6d4106d041ab0780d2aa8f722bc4a2a6618
Naeem Ahmed <naeem.ahmedk@outlook.com>
2020-07-06 01:52:35 +0500

    Initial

---
+++ index.js
@@ -0,0 +1,33 @@
+     require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const crypto = require('crypto')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(bodyParser.json(), cors())
+app.options('*', cors());
+
+app.post('/signature', (req, res) => {
+
+    const timestamp = new Date().getTime() - 30000
+    const msg = Buffer.from(process.env.API_KEY + req.body.meetingNumber + timestamp + req.body.role).toString('base64')
+    const hash = crypto.createHmac('sha256', process.env.API_SECRET).update(msg).digest('base64')
+    const signature = Buffer.from(`${process.env.API_KEY}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64')
+
+    res.json({
+        signature: signature,
+        req.body.meetingNumber,
+        req.body.role
+    })
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        message:"Hello App was Created"
+    })
+})
+
+app.listen(port, () => console.log(`Zoom Web SDK Sample Signature Node.js on port ${port}!`))
